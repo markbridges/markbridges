bef6076579970ec74e348d87976b036e1327ea41
Mark Bridges <mebridges@gmail.com>
2017-10-17 23:03:02 -0400

    Updated about me page.

--- pages/about.vue
+++ pages/about.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>About Me</h1>
     <br/>
-    <p>This is about me. Well, not really.  It's just a site for playing with
-    various technologies and documenting results.</p>
+    <p>This is about me. Well, not really about me.  It's just a site for playing with
+    various technologies and documenting results. Go Dawgs!</p>
   </div>
 
 </template>
